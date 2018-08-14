add0ce04ec32a9d8f703fcb9b438880b93eb7a39
Hayden <33820650+sirredbeard@users.noreply.github.com>
2018-08-14 02:03:33 -0400

    Added install wslu

--- create-targz.sh
+++ create-targz.sh
@@ -8,12 +8,17 @@ TMPDIR=$(mktemp -d)
 ARCH="amd64"
 DIST="stable"
 cd $TMPDIR
-sudo cdebootstrap -a $ARCH --include=sudo,locales,git,python3 $DIST $DIST http://deb.debian.org/debian
+sudo cdebootstrap -a $ARCH --include=sudo,locales,git,python3,apt-transport-https $DIST $DIST http://deb.debian.org/debian
 # clean apt cache
 sudo chroot $DIST apt-get clean
 # configure bash
 sudo chroot $DIST /bin/bash -c "echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen && locale-gen"
 sudo chroot $DIST /bin/bash -c "update-locale LANGUAGE=en_US.UTF-8 LC_ALL=C"
+# install wslu
+sudo chroot $DIST wget -O - https://api.patrickwu.ml/public.key | sudo apt-key add -
+sudo chroot $DIST echo "deb https://apt.patrickwu.ml/ stretch main" | sudo tee -a /etc/apt/sources.list 
+sudo chroot $DIST sudo apt update
+sudo chroot $DIST apt install wslu -y
 # copy custom files to image
 sudo cp $BUILDIR/linux_files/profile $TMPDIR/$DIST/etc/profile
 sudo cp $BUILDIR/linux_files/os-release $TMPDIR/$DIST/etc/os-release
