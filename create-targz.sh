a7020a8f24a0d584dec50fe07a92210a2dcec7eb
Hayden <33820650+sirredbeard@users.noreply.github.com>
2018-08-14 02:57:23 -0400

    Update create-targz.sh

--- create-targz.sh
+++ create-targz.sh
@@ -15,10 +15,6 @@ sudo chroot $DIST apt-get clean
 # configure bash
 sudo chroot $DIST /bin/bash -c "echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen && locale-gen"
 sudo chroot $DIST /bin/bash -c "update-locale LANGUAGE=en_US.UTF-8 LC_ALL=C"
-# install wslu repo key
-sudo chroot $DIST wget https://api.patrickwu.ml/public.key --no-check-certificate
-sudo chroot $DIST apt-key add public.key
-sudo chroot $DIST rm public.key
 # copy custom files to image
 sudo cp $BUILDIR/linux_files/profile $TMPDIR/$DIST/etc/profile
 sudo cp $BUILDIR/linux_files/os-release $TMPDIR/$DIST/etc/os-release
@@ -26,9 +22,11 @@ sudo cp $BUILDIR/linux_files/sources.list $TMPDIR/$DIST/etc/apt/sources.list
 # copy app installer scripts to image
 sudo cp $BUILDIR/linux_files/installchrome.sh $TMPDIR/$DIST/opt/installchrome.sh
 sudo cp $BUILDIR/linux_files/installcode.sh $TMPDIR/$DIST/opt/installcode.sh
+sudo cp $BUILDIR/linux_files/installcode.sh $TMPDIR/$DIST/opt/installwslu.sh
 # make app installer scripts executable
 sudo chroot $DIST chmod u+x /opt/installchrome.sh
 sudo chroot $DIST chmod u+x /opt/installcode.sh
+sudo chroot $DIST chmod u+x /opt/installwslu.sh
 # remove unnecessary apt packages
 sudo chroot $DIST apt remove systemd dmidecode -y --allow-remove-essential
 # clean up orphaned apt dependencies
