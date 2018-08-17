2d4a776b43efc3c711eee96055ab59c0a68acf1f
Hayden <33820650+sirredbeard@users.noreply.github.com>
2018-08-17 11:38:59 -0400

    Update installwslu.sh

--- linux_files/installwslu.sh
+++ linux_files/installwslu.sh
@@ -3,5 +3,6 @@
 # see also install script https://github.com/patrick330602/wslu/blob/master/scripts/install.sh
 sudo apt install lsb-release apt-transport-https
 wget -O - https://api.patrickwu.ml/public.key | sudo apt-key add -
-sudo echo "deb https://apt.patrickwu.ml/ `lsb_release -c -s` main" >> /etc/apt/sources.list 
+echo "deb https://apt.patrickwu.ml/ `lsb_release -c -s` main" | sudo tee -a /etc/apt/sources.list 
 sudo apt update
+sudo apt install wslu
