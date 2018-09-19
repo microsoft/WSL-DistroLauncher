dae95b293d65ae97041c1d2bdd53bd95b93f8311
Hayden <33820650+sirredbeard@users.noreply.github.com>
2018-09-19 10:58:22 -0400

    Create installchrome.sh

---
+++ linux_files/installchrome.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main " > /etc/apt/sources.list.d/googlechrome.list' 
+sudo apt upgrade -y 
+sudo apt update -y  
+sudo apt install google-chrome-stable
