f1e2663a10c737ec803b8525e8817b9e6c3a885b
Hayden <33820650+sirredbeard@users.noreply.github.com>
2018-09-19 10:52:56 -0400

    Delete installchrome.sh

--- linux_files/installchrome.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main " > /etc/apt/sources.list.d/googlechrome.list'
-sudo apt upgrade -y
-sudo apt update -y 
-sudo apt install google-chrome-stable
