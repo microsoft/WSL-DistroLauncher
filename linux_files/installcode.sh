07fa59f2413f2c1bded4b27a2331e19af25d4d6d
Hayden <33820650+sirredbeard@users.noreply.github.com>
2018-09-19 10:59:26 -0400

    Create installcode.sh

---
+++ linux_files/installcode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+sudo apt-get install apt-transport-https curl -y 
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg 
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg 
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' 
+sudo apt upgrade -y 
+sudo apt update -y 
+sudo apt install code
