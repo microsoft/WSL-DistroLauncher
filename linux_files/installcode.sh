b6cef90dfcba50cae686ef7f358a8b1b9808483b
Hayden <contact@whitewaterfoundry.com>
2018-09-19 20:15:29 -0400

    Added dependencies

--- linux_files/installcode.sh
+++ linux_files/installcode.sh
@@ -6,4 +6,4 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' 
 sudo apt upgrade -y 
 sudo apt update -y 
-sudo apt install code
+sudo apt install code libxss1 libasound2
