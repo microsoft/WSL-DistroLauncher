304020ce98ea1e591d1678f57f6846939aae7214
DerekStodghill <derekstodghill@gmail.com>
2021-02-12 16:20:34 -0600

    Fixed for CentOS7

--- DistroLauncher/DistributionInfo.cpp
+++ DistroLauncher/DistributionInfo.cpp
@@ -9,7 +9,7 @@ bool DistributionInfo::CreateUser(std::wstring_view userName)
 {
     // Create the user account.
     DWORD exitCode;
-    std::wstring commandLine = L"/usr/sbin/adduser --quiet --gecos '' ";
+    std::wstring commandLine = L"/usr/sbin/adduser ";
     commandLine += userName;
     HRESULT hr = g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
     if ((FAILED(hr)) || (exitCode != 0)) {
@@ -17,17 +17,17 @@ bool DistributionInfo::CreateUser(std::wstring_view userName)
     }
 
     // Add the user account to any relevant groups.
-    commandLine = L"/usr/sbin/usermod -aG adm,cdrom,sudo,dip,plugdev ";
-    commandLine += userName;
-    hr = g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
-    if ((FAILED(hr)) || (exitCode != 0)) {
+    //commandLine = L"/usr/sbin/usermod -aG adm,cdrom,sudo,dip,plugdev ";
+	//commandLine += userName;
+	//hr = g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
+	//if ((FAILED(hr)) || (exitCode != 0)) {
 
         // Delete the user if the group add command failed.
-        commandLine = L"/usr/sbin/deluser ";
-        commandLine += userName;
-        g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
-        return false;
-    }
+		//commandLine = L"/usr/sbin/deluser ";
+		//commandLine += userName;
+		//g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
+		//return false;
+	//}
 
     return true;
 }
