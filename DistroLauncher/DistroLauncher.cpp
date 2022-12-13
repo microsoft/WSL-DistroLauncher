b9fa755f80c15b8cd4a5fa9c1678ce706a87570f
Ben Hillis (518152) <Ben.Hillis@microsoft.com>
2022-12-13 14:15:28 -0800

    Change WSL to use system error string for WSL OC not present

--- DistroLauncher/DistroLauncher.cpp
+++ DistroLauncher/DistroLauncher.cpp
@@ -86,7 +86,7 @@ int wmain(int argc, wchar_t const *argv[])
     // Ensure that the Windows Subsystem for Linux optional component is installed.
     DWORD exitCode = 1;
     if (!g_wslApi.WslIsOptionalComponentInstalled()) {
-        Helpers::PrintMessage(MSG_MISSING_OPTIONAL_COMPONENT);
+        Helpers::PrintErrorMessage(HRESULT_FROM_WIN32(ERROR_LINUX_SUBSYSTEM_NOT_PRESENT));
         if (arguments.empty()) {
             Helpers::PromptForInput();
         }
@@ -156,10 +156,7 @@ int wmain(int argc, wchar_t const *argv[])
 
     // If an error was encountered, print an error message.
     if (FAILED(hr)) {
-        if (hr == HRESULT_FROM_WIN32(ERROR_LINUX_SUBSYSTEM_NOT_PRESENT)) {
-            Helpers::PrintMessage(MSG_MISSING_OPTIONAL_COMPONENT);
-
-        } else if (hr == HCS_E_HYPERV_NOT_INSTALLED) {
+        if (hr == HCS_E_HYPERV_NOT_INSTALLED) {
             Helpers::PrintMessage(MSG_ENABLE_VIRTUALIZATION);
 
         } else {
